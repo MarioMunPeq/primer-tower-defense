5f304fca3834f99f13cded170e17cb7b754fd662
MarioMunPeq <snakeyes271101@gmail.com>
2026-09-14 13:49:27 +0200

    VFX: drastically reduce impact spark and ring effects

--- scripts/effects/impact_effect.gd
+++ scripts/effects/impact_effect.gd
@@ -6,12 +6,12 @@ var _tween: Tween = null
 
 func _ready() -> void:
 	_tween = create_tween()
-	_tween.tween_property(self, "modulate:a", 0.0, 0.2)
-	_tween.tween_property(self, "scale", Vector2(2.0, 2.0), 0.2)
+	_tween.tween_property(self, "modulate:a", 0.0, 0.18)
+	_tween.tween_property(self, "scale", Vector2(1.3, 1.3), 0.18)
 	_tween.finished.connect(_on_finished)
 
 func _draw() -> void:
-	draw_circle(Vector2.ZERO, 8.0, Color(1.0, 0.9, 0.4, 0.8), false, 2.0)
+	draw_circle(Vector2.ZERO, 5.0, Color(1.0, 0.9, 0.4, 0.5), false, 1.0)
 
 func _on_finished() -> void:
 	queue_free()
